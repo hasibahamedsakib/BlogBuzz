bf951ff02ea584514ba2f2af5135f08eeb019da6
ijaz35 <ijaz.hossain1@gmail.com>
2023-07-05 00:58:07 +0600

    footer updated

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './footer.module.css'
+import Image from 'next/image';
+const Footer = () => {
+    return (
+        <div className={styles.container}>
+            <div>©2023 Lamamia. All rights reserved.</div>
+            <div className={styles.social}>
+                <Image src="/1.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+                <Image src="/2.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+                <Image src="/3.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+                <Image src="/4.png" width={15} height={15} className={styles.icon} alt="lama dev" />
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
