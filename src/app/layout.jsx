ddc1fa356dbdd53da7ea451cf055af7cddd94530
ijaz35 <ijaz.hossain1@gmail.com>
2023-07-04 23:54:53 +0600

    light and dark mode added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,9 @@
 import Navbar from '@/components/navbar/Navbar'
 import './globals.css'
 import { Inter, Poppins } from 'next/font/google'
+import { ThemeProvider } from './ThemeContext/ThemeProvider'
 // import Footer from '@/components/footer/Footer'
-// import { ThemeProvider } from '@/context/ThemeContext' 
+
 
 const Poppin = Poppins({ subsets: ['latin'], weight: ['300', '400', '600', '900'] })
 
@@ -15,13 +16,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={Poppin.className}>
-        {/* <ThemeProvider> */}
+        <ThemeProvider>
           <div className="container max-w-7xl min-h-screen my-0 mx-auto px-12 flex flex-col justify-between">
             <Navbar></Navbar>
             {children}
             {/* <Footer></Footer> */}
           </div>
-        {/* </ThemeProvider> */}
+        </ThemeProvider>
       </body>
     </html>
   )
---
+++ src/components/DarkModeToggle/DarkModeToggle.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import styles from './page.module.css'
+import { ThemeContext } from '@/app/ThemeContext/ThemeProvider';
+
+
+const DarkModeToggle = () => {
+    const { toggle, mode } = useContext(ThemeContext)
+    return (
+        <div className={styles.container} onClick={toggle}>
+            <div className={styles.icon}>🌙</div>
+            <div className={styles.icon}>🔆</div>
+            <div className={styles.ball} style={mode === "light" ? { left: "2px" } : { right: "2px" }}></div>
+        </div>
+    );
+};
+
+export default DarkModeToggle;
